efe994afcd3d42e0488114264fae385397f8a290
takaya <s.takaya1027@gmail.com>
2018-12-24 23:58:08 +0000

    ユーザー認証追加とか

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,43 @@
 class UsersController < ApplicationController
   def show
+    @user  = User.find_by(id: params[:id])
   end
   
   def signup
+    @user = User.new()
+  end
+  
+  def login_form
+    @user = User.new()
+  end  
+  
+  def logout
+    session[:user_id] = nil
+    redirect_to('/')
   end
   
   def login
-  end  
+    @user = User.find_by(email: params[:email],password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to("/users/show/#{@user.id}")
+    else
+      @error_massage = "password or email is different !!"
+      render("users/login_form")
+    end
+  end
+  
+  def create
+      @user = User.new(email: params[:email],name: params[:name],password: params[:password])
+    
+    if params[:password] == params[:confirmation_password] && @user.save
+      @user.save
+      redirect_to("/users/show")
+    else
+      render("users/signup")
+    end
+    
+  end
   
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  post 'users/create' => "users#create"
+  post 'users/logout'  => 'users#logout'
+  post 'users/login'  => 'users#login'
+  get 'users/signup' => "users#signup"
+  get 'users/login_form'  => "users#login_form"
+  get 'users/show/:id'  => "users#show"
   get '/' => "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
